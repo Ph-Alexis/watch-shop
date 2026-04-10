fb91556769c228e984ed537bbe833af8edcca34e
Q-Tram <quynhtraam19@gmail.com>
2026-04-10 12:02:18 +0700

    Fix formatting for updateProductApi function

--- watch-store-client/src/api/productApi.js
+++ watch-store-client/src/api/productApi.js
@@ -3,6 +3,5 @@ import axiosClient from "../utils/axiosClient";
 export const getProductsApi = () => axiosClient.get("/products");
 export const getProductByIdApi = (id) => axiosClient.get(`/products/${id}`);
 export const createProductApi = (data) => axiosClient.post("/products", data);
-export const updateProductApi = (id, data) =>
-  axiosClient.put(`/products/${id}`, data);
+export const updateProductApi = (id, data) => axiosClient.put(`/products/${id}`, data);
 export const deleteProductApi = (id) => axiosClient.delete(`/products/${id}`);
