1f3c1505cad49538ac4e4033a22b2a7764613634
Q-Tram <quynhtraam19@gmail.com>
2026-04-10 12:13:08 +0700

    Update main.jsx

--- watch-store-client/src/main.jsx
+++ watch-store-client/src/main.jsx
@@ -6,15 +6,18 @@ import App from "./App";
 import "./index.css";
 import { AuthProvider } from "./context/AuthContext";
 import { CartProvider } from "./context/CartContext";
+import { WebsiteSettingsProvider } from "./context/WebsiteSettingsContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <HelmetProvider>
       <BrowserRouter>
         <AuthProvider>
-          <CartProvider>
-            <App />
-          </CartProvider>
+          <WebsiteSettingsProvider>
+            <CartProvider>
+              <App />
+            </CartProvider>
+          </WebsiteSettingsProvider>
         </AuthProvider>
       </BrowserRouter>
     </HelmetProvider>
