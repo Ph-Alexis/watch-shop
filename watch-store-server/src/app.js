d80b436d9852ff1dab4ee91fe74feb5d80f86e2d
Q-Tram <quynhtraam19@gmail.com>
2026-04-10 12:21:02 +0700

    Update app.js

--- watch-store-server/src/app.js
+++ watch-store-server/src/app.js
@@ -7,6 +7,7 @@ const categoryRoutes = require("./routes/categoryRoutes");
 const orderRoutes = require("./routes/orderRoutes");
 const userRoutes = require("./routes/userRoutes");
 const adminRoutes = require("./routes/adminRoutes");
+const settingRoutes = require("./routes/settingRoutes");
 
 const notFoundMiddleware = require("./middleware/notFoundMiddleware");
 const errorMiddleware = require("./middleware/errorMiddleware");
@@ -20,8 +21,8 @@ app.use(
   }),
 );
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "100mb" }));
+app.use(express.urlencoded({ extended: true, limit: "100mb" }));
 
 app.get("/", (req, res) => {
   res.send("Watch Store API is running");
@@ -32,7 +33,9 @@ app.use("/api/products", productRoutes);
 app.use("/api/categories", categoryRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/customers", userRoutes);
 app.use("/api/admin", adminRoutes);
+app.use("/api/settings", settingRoutes);
 
 app.use(notFoundMiddleware);
 app.use(errorMiddleware);
