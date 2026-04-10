1f5bf74d980dc1f6aae721717dc24cc39f0e5c93
Q-Tram <quynhtraam19@gmail.com>
2026-04-10 12:19:55 +0700

    Update userRoutes.js

--- watch-store-server/src/routes/userRoutes.js
+++ watch-store-server/src/routes/userRoutes.js
@@ -5,12 +5,17 @@ const {
   getProfile,
   updateProfile,
   changePassword,
+  getAllUsers,
+  toggleLock,
 } = require("../controllers/userController");
 
 const protect = require("../middleware/authMiddleware");
+const isAdmin = require("../middleware/adminMiddleware");
 
 router.get("/profile", protect, getProfile);
 router.put("/profile", protect, updateProfile);
 router.patch("/change-password", protect, changePassword);
+router.get("/", protect, isAdmin, getAllUsers);
+router.patch("/:id/toggle-lock", protect, isAdmin, toggleLock);
 
 module.exports = router;
