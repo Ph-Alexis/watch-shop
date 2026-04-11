d497606fd6c35d92a5dea85a2b3afbc3b1ef6293
Q-Tram <quynhtraam19@gmail.com>
2026-04-11 23:45:56 +0700

    Update paymentSettingApi.js

--- watch-store-client/src/api/paymentSettingApi.js
+++ watch-store-client/src/api/paymentSettingApi.js
@@ -1,3 +1,6 @@
 import axiosClient from "../utils/axiosClient";
 
 export const getPaymentSettingApi = () => axiosClient.get("/payment-setting");
+
+export const updatePaymentSettingApi = (payload) =>
+  axiosClient.put("/payment-setting", payload);
